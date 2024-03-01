a444b5f16534a5458e3eb9f09991a8b01846e944
Mitzy Diaz Valor <mitzy_diaz_valor@outlook.com>
2024-02-29 19:16:28 -0600

    feat(NotFound.tsx): add styles

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Header } from "../components/Header";
+import { Container, Typography, Box } from '@mui/material'
 
 export const NotFound = () => {
-    return <div>
-    NF Page
+    return <div style={{marginTop:'1rem'}}><Header/>
+    <Typography variant='h2'>Page Not Found</Typography>
     </div>;
 };
